692aba9ef4b6d8d35bea96d10dc5147169521ecb
unknown <pdpravin47@gmail.com>
2021-03-14 08:14:15 +0530

    dotenv added

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 Promise = require('bluebird'); // eslint-disable-line no-global-assign
 // const express = require('express');
 // const helmet = require("helmet");
+require('dotenv').config();
 require("./config/connection");
 const app = require('./config/express');
 // const studentRouter = require("./api/controllers/student");
@@ -14,7 +15,7 @@ const mongoose = require('./config/mongoose');
 // app.use(helmet());
 // var cors = require('cors');
 // const { Router } = require('express');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 // app.use(express.json());
 // app.use(cors());
